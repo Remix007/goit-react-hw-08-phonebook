3fc347296187b5a18e94a5ecf64254af2a97d854
Remix007 <forvard16back@gmail.com>
2023-03-23 15:23:09 +0200

    navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,25 @@
+import UserMenu from '../UserMenu/UserMenu';
+import { useSelector } from 'react-redux';
+import authSelectors from 'redux/selectors';
+import { AuthBox, Header, Link, Title } from './Navigation.styled';
+
+const Navigation = () => {
+  const isLoggedIn = useSelector(authSelectors.getIsloggedIn);
+  return (
+    <Header>
+      <Title>PhoneBook</Title>
+      {!isLoggedIn ? (
+        <>
+          <AuthBox>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </AuthBox>
+        </>
+      ) : (
+        <UserMenu />
+      )}
+    </Header>
+  );
+};
+
+export default Navigation;
