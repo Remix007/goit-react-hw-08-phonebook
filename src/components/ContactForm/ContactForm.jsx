5635fdb955233671e6c4a69e72eeebd903f1099a
Remix007 <forvard16back@gmail.com>
2023-03-23 02:08:18 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -30,6 +30,37 @@ const ContactsForm = () => {
     });
   };
 
-  
+  return (
+    <form action="submit" onSubmit={handleFormSubmit}>
+      <label htmlFor="name">
+        <p className={css.label}>Name</p>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label htmlFor="number">
+        <p className={css.label}>Number</p>
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <button type="submit" className={css.btn}>
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 export default ContactsForm;
